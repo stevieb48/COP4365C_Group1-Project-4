1025d354de36c380420a6387416885826e967dd6
tmb54 <38083711+tmb54@users.noreply.github.com>
2018-04-18 18:07:01 -0500

    Add files via upload
    Never done this before, I hope it works like this?? lol.

---
+++ Group 1 Project 4 Exercise 19.4/Group 1 Project 4 Exercise 19.4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Group_1_Project_4_Exercise_19._4
+{
+    public class StackExercise
+    {
+        public static void Main(string[] args)
+        {
+            //create stack
+            Stack stack = new Stack();
+
+            //create character array to hold string
+            char[] textLine;
+
+            //tell user what program does
+            Console.WriteLine("Enter any string and this program will reverse it!");
+
+            //read in user input
+            textLine = Console.ReadLine().ToCharArray();
+
+            //add user input to stack
+            foreach (char c in textLine)
+            {
+                stack.Push(c);
+            }
+
+            //confirm user entry
+            Console.WriteLine("\nYou entered: ");
+
+            //show user what they entered
+            foreach (char c in textLine)
+            {
+                Console.Write(c);
+            }
+
+            
+            Console.WriteLine("\n\nNow here is the statement reversed!");
+
+            //reverse the user's statement by popping off of the top of the stack.
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            //holds the console open so it can be read until a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
